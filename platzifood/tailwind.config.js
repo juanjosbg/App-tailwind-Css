4d5b29cea41d3435cfad8c8eb8bbdc7685fd0105
juanjosbg <juanjoseborrero95@gmail.com>
2022-06-23 18:47:27 -0500

    second script

--- platzifood/tailwind.config.js
+++ platzifood/tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    fontFamily: {
+      display: ['Merriweather', 'serif'],
+      body: ['Graphik', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        'color1': '#5F7161',
+        'color2': '#6D8B74',
+        'color3': '#EFEAD8',
+        'color4': '#D0C9C0',
+        'color5': '#E0D8B0',
+        'color6': '#F8ECD1',
+      },
+      margin: {
+        'mt': '6rem',
+        'mb': '6em',
+      },
+    },
   },
   plugins: [],
 }
